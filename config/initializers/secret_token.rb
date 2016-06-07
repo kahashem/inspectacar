33f58b38dab45b3ed29d091f80b9025f2397b5a8
kahashem <kahashem@outlook.com>
2016-06-07 18:05:21 +0000

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inspectacar::Application.config.secret_key_base = 'c87b893be9f2c91d61f4fab9d9afe1306a40c92e2fe7f850cb94fe2a5db5ba05fad895a548213be59e288b3a527b21cd9a700c08ce62ad8fd73a175aec5744ff'
